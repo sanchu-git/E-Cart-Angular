5c5a2f1b6712d225d323f5f08533d0832227f09b
sanchu-git <sanchuhareesh@gmail.com>
2024-03-02 23:01:22 +0530

    Cart Cmplt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllProductsComponent } from './all-products/all-products.component';
+import { ViewProductComponent } from './view-product/view-product.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import {UserCartComponent} from './user-cart/user-cart.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component:AllProductsComponent },
+  { path:'view/:id', component:ViewProductComponent },
+  { path:'login', component:LoginComponent },
+  { path:'register', component:RegisterComponent },
+  { path:'wishlist', component:WishlistComponent },
+  { path:'cart', component:UserCartComponent },
+  { path:'**', redirectTo:'' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AllProductsComponent } from './all-products/all-products.component';
+import { ViewProductComponent } from './view-product/view-product.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { UserCartComponent } from './user-cart/user-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    AllProductsComponent,
+    ViewProductComponent,
+    WishlistComponent,
+    UserCartComponent,
   ],
   imports: [
     BrowserModule,
